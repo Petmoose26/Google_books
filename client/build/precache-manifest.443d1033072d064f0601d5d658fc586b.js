b3a115f97bf142f60888e0db76d51d24f1a4d578
James Louis <jlouis1026@gmail.com>
2020-10-14 19:42:11 -0400

    deploy app

---
+++ client/build/precache-manifest.443d1033072d064f0601d5d658fc586b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5082c3555879b06756edbf3232f4a08d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31b4c3d97c6d23adcad9",
+    "url": "/static/css/main.c6fab11a.chunk.css"
+  },
+  {
+    "revision": "d6a15e8fae7c7663a9c1",
+    "url": "/static/js/2.25dc9d6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.25dc9d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b4c3d97c6d23adcad9",
+    "url": "/static/js/main.552d2557.chunk.js"
+  },
+  {
+    "revision": "c46c97987879c7eab527",
+    "url": "/static/js/runtime-main.8dd9c097.js"
+  }
+]);
